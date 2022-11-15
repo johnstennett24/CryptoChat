25c76cb2310849df6be5f132a5fdea4121509d71
johnstennett24 <93450112+johnstennett24@users.noreply.github.com>
2022-11-15 16:30:50 -0700

    addes stuff

--- lib/CryptoPrice.dart
+++ lib/CryptoPrice.dart
@@ -1,3 +1,12 @@
+/// This class is used for storing prices and the time associated
+/// with that price from the API. The DataServices class has a method
+/// getMarketCharts and addToList. They are meant to be used together.
+/// getMarketCharts returns a Map<String, dynamic> and one of the keys of
+/// that map is "prices", the value is a list of lists of prices at index of 1
+/// and time at index of 0, the addToList method creates and returns a list of
+/// type CryptoPrice that then is used in the CryptoChart widget as the x
+/// and y values for each data point.
+
 class CryptoPrice {
   CryptoPrice(this.time, this.price);
   final int time;
